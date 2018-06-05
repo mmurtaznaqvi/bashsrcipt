be894fb87c10de1525f14ad92d1e6513c5158cc0
mmurtaznaqvi <murtazamirnaqvi@gmail.com>
2018-06-05 10:14:50 +0500

    functionsadded

--- ovs.sh
+++ ovs.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
-while [ 1 ]
-do
-	UPSTREAM=${1:-'@{u}'}
-	LOCAL=$(git rev-parse @)	
-	REMOTE=$(git rev-parse "$UPSTREAM")
-	BASE=$(git merge-base @ "$UPSTREAM")
 
-	if [ $LOCAL = $REMOTE ]; then
-    		echo "Up-to-date"
-	elif [ $LOCAL = $BASE ]; then
-         
-         echo "Step 1. Get Updates"
+
+install_openvswitch(){
+        echo "Step 1. Get Updates"
         git fetch
+        cd ..
 
         echo "Step 2. Clone Openvswitch git Repo"
         git clone https://github.com/openvswitch/ovs.git
@@ -39,17 +32,39 @@ do
                  --pidfile --detach
 		    sudo ovs-vsctl --no-wait init
 		    sudo ovs-vswitchd --pidfile --detach
+}
+
+
+
+cd ovs/
+while [ 1 ]
+do
+	UPSTREAM=${1:-'@{u}'}
+	LOCAL=$(git rev-parse @)	
+	REMOTE=$(git rev-parse "$UPSTREAM")
+	BASE=$(git merge-base @ "$UPSTREAM")
+
+	if [ $LOCAL = $REMOTE ]; then
+    		echo "Up-to-date"
+        break
+	elif [ $LOCAL = $BASE ]; then
+         install_openvswitch
+   
 	
 	fi
 done
 
-for i in {1..100}
+rules() {
+sudo ovs-ofctl del-flows br1
+for i in $(seq 1 100)
 do
-  ip=192.168.1.${i}
-  portOut=$(( RANDOM % (4 - 1 + 1 ) + 1 ))
-  echo $ip
-  sudo ovs-ofctl add-flow br1 dl_type=0x0800,nw_dst=$ip,actions=output:$portOut
+ip=192.168.1.${i}
+portOut=$(( RANDOM % (4 - 1 + 1 ) + 1 ))
+echo portOut
+echo $ip
+sudo ovs-ofctl add-flow br1 dl_type=0x0800,nw_dst=$ip,actions=output:$portOut
 done
-
-
-
+  
+}
+rules
+sudo ovs-ofctl dump-flows br1
